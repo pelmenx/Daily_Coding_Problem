fb4bb834fd3d2e9d2e01967b0fcc3b20665298c5
pelmenx <vtalantsev@gmail.com>
2021-10-03 22:47:39 +0300

    update

--- Solutions/Daily Coding Problem: Problem #227 [Easy].py
+++ Solutions/Daily Coding Problem: Problem #227 [Easy].py
@@ -11,3 +11,59 @@
 # --------------------------------------------------------------------------------
 #
 #
+class Boggle(object):
+    def __init__(self):
+        super(Boggle, self).__init__()
+        self.board = [['e', 'i', 'p', 'k'],
+                      ['g', 'l', 'a', 'm'],
+                      ['a', 'e', 'u', 'f'],
+                      ['d', 'o', 'p', 'o']]
+        self.row_number = 4
+        self.col_number = 4
+
+        self.valid_words_list = {"dope", "gale", "pale", "pile", "lie", "gad", "dag", "pie", "male", "fuel", "pod"}
+
+
+def solve_boggle_game(boggle):
+    def solve(i, j, word):
+        if len(word) >= 3:
+            yield word
+        visited.add((i, j))
+        if i + 1 < boggle.row_number and (i + 1, j) not in visited:
+            yield from solve(i + 1, j, word + boggle.board[i + 1][j])
+            visited.remove((i + 1, j))
+        if i - 1 >= 0 and (i - 1, j) not in visited:
+            yield from solve(i - 1, j, word + boggle.board[i - 1][j])
+            visited.remove((i - 1, j))
+        if j + 1 < boggle.col_number and (i, j + 1) not in visited:
+            yield from solve(i, j + 1, word + boggle.board[i][j + 1])
+            visited.remove((i, j + 1))
+        if j - 1 >= 0 and (i, j - 1) not in visited:
+            yield from solve(i, j - 1, word + boggle.board[i][j - 1])
+            visited.remove((i, j - 1))
+        if i + 1 < boggle.row_number and j + 1 < boggle.col_number and (i + 1, j + 1) not in visited:
+            yield from solve(i + 1, j + 1, word + boggle.board[i + 1][j + 1])
+            visited.remove((i + 1, j + 1))
+        if i + 1 < boggle.row_number and j - 1 >= 0 and (i + 1, j - 1) not in visited:
+            yield from solve(i + 1, j - 1, word + boggle.board[i + 1][j - 1])
+            visited.remove((i + 1, j - 1))
+        if i - 1 >= 0 and j + 1 < boggle.col_number and (i - 1, j + 1) not in visited:
+            yield from solve(i - 1, j + 1, word + boggle.board[i - 1][j + 1])
+            visited.remove((i - 1, j + 1))
+        if i - 1 >= 0 and j - 1 >= 0 and (i - 1, j - 1) not in visited:
+            yield from solve(i - 1, j - 1, word + boggle.board[i - 1][j - 1])
+            visited.remove((i - 1, j - 1))
+
+    visited = set()
+    words = set()
+    for i_ in range(boggle.row_number):
+        for j_ in range(boggle.col_number):
+            for word_ in solve(i_, j_, boggle.board[i_][j_]):
+                if word_ in boggle.valid_words_list:
+                    words.add(word_)
+            visited.remove((i_, j_))
+    return words
+
+
+boggle = Boggle()
+assert solve_boggle_game(boggle) == boggle.valid_words_list
