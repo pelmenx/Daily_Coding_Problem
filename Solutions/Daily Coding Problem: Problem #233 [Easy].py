5ffd5df33612aa5e2c6949aa3e2093f0ab14ef40
pelmenx <vtalantsev@gmail.com>
2021-10-24 15:50:00 +0300

    add #233

--- Solutions/Daily Coding Problem: Problem #233 [Easy].py
+++ Solutions/Daily Coding Problem: Problem #233 [Easy].py
@@ -9,3 +9,24 @@
 # --------------------------------------------------------------------------------
 #
 #
+def fib(n: int) -> int:
+    if n > 0:
+        if n == 1:
+            return 0
+        elif n == 2:
+            return 1
+        else:
+            fib_que = [0, 1]
+            for _ in range(2, n):
+                fib_que = fib_que[-1:] + [sum(fib_que)]
+            return fib_que[-1]
+
+
+assert fib(1) == 0
+assert fib(2) == 1
+assert fib(3) == 1
+assert fib(4) == 2
+assert fib(5) == 3
+assert fib(6) == 5
+assert fib(7) == 8
+assert fib(8) == 13
