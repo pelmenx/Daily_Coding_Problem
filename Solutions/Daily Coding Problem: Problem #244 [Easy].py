8fd9a9804e2cd1f7dcd917a8604680936d4b1872
pelmenx <vtalantsev@gmail.com>
2021-11-01 14:32:10 +0300

    add #244

--- Solutions/Daily Coding Problem: Problem #244 [Easy].py
+++ Solutions/Daily Coding Problem: Problem #244 [Easy].py
@@ -4,7 +4,7 @@
 #
 # The Sieve of Eratosthenes is an algorithm used to generate all prime numbers
 # smaller than N. The method is to take increasingly larger prime numbers, and
-# mark their multiples as composite. 
+# mark their multiples as composite.
 #
 # For example, to find all primes less than 100, we would first mark [4, 6, 8,
 # ...] (multiples of two), then [6, 9, 12, ...] (multiples of three), and so on.
@@ -20,3 +20,35 @@
 # --------------------------------------------------------------------------------
 #
 #
+def Sieve_of_Eratosthenes(N):
+    numbers_set = set([i for i in range(2, N)])
+    for i in range(2, N // 2 + 1):
+        j = 2
+        while j*i < N:
+            numbers_set.discard(j * i)
+            j += 1
+    return numbers_set
+
+
+def next_prime_number(prime_numbers):
+    possible_number = prime_numbers[-1]+1
+    yield prime_numbers[-1]
+    while True:
+        for prime in prime_numbers:
+            if possible_number % prime == 0:
+                break
+        else:
+            prime_numbers.append(possible_number)
+            yield possible_number
+        possible_number += 1
+
+
+def produce_prime_numbers():
+    prime_numbers = [2]
+    yield from next_prime_number(prime_numbers)
+
+
+primes = Sieve_of_Eratosthenes(100)
+next_prime = produce_prime_numbers()
+for prime in primes:
+    assert prime == next(next_prime)
