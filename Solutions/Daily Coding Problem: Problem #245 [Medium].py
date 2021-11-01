578abfa223cee7e4072687410d69ae709e9bbc59
pelmenx <vtalantsev@gmail.com>
2021-11-01 18:53:04 +0300

    add #245

--- Solutions/Daily Coding Problem: Problem #245 [Medium].py
+++ Solutions/Daily Coding Problem: Problem #245 [Medium].py
@@ -14,3 +14,22 @@
 # --------------------------------------------------------------------------------
 #
 #
+def find_number_of_steps(array: list[int]) -> int:
+    def find_min_number_of_steps_inside(position, number_of_steps=0):
+        if number_of_steps >= min_number_of_steps:
+            return
+        if position == len(array) - 1:
+            yield number_of_steps
+        for i in range(1, array[position] + 1):
+            if position + i < len(array):
+                yield from find_min_number_of_steps_inside(position + i, number_of_steps + 1)
+    min_number_of_steps = len(array) + 1
+    for step_number in find_min_number_of_steps_inside(0):
+        if step_number < min_number_of_steps:
+            min_number_of_steps = step_number
+    return min_number_of_steps if min_number_of_steps <= len(array) else None
+
+
+assert find_number_of_steps([6, 2, 4, 0, 5, 1, 1, 4, 2, 9]) == 2
+assert find_number_of_steps([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]) == 9
+assert find_number_of_steps([1, 0, 0, 0, 0, 0, 0, 0, 0, 1]) is None
