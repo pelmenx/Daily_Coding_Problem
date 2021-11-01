f4e0392180a56a59191c93a3cccf52a234d62f4c
pelmenx <vtalantsev@gmail.com>
2021-11-01 20:01:35 +0300

    add #247

--- Solutions/Daily Coding Problem: Problem #247 [Easy].py
+++ Solutions/Daily Coding Problem: Problem #247 [Easy].py
@@ -10,3 +10,52 @@
 # --------------------------------------------------------------------------------
 #
 #
+from sys import maxsize
+
+
+class Node:
+    def __init__(self, arg):
+        self.arg = arg
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        return repr(self.arg)
+
+
+def is_binary_tree_height_balanced(root: Node) -> bool:
+    def is_binary_tree_height_balanced_helper(root_: Node, level: int) -> None:
+        if not root_.right and not root_.left:
+            yield level
+        if root_.left:
+            yield from is_binary_tree_height_balanced_helper(root_.left, level + 1)
+        if root_.right:
+            yield from is_binary_tree_height_balanced_helper(root_.right, level + 1)
+
+    min_level = maxsize
+    max_level = -maxsize
+    for high in is_binary_tree_height_balanced_helper(root, 0):
+        if high < min_level:
+            min_level = high
+        if high > max_level:
+            max_level = high
+    return True if (max_level - min_level) == 1 or max_level == min_level else False
+
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+d = Node(4)
+e = Node(5)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+
+assert is_binary_tree_height_balanced(a) is True
+
+f = Node(6)
+d.left = f
+
+assert is_binary_tree_height_balanced(a) is False
