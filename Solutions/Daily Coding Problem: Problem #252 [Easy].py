c82548e213b62ec4e3daeaa079a78618c37ea4af
pelmenx <vtalantsev@gmail.com>
2021-11-03 23:39:33 +0300

    add #252

--- Solutions/Daily Coding Problem: Problem #252 [Easy].py
+++ Solutions/Daily Coding Problem: Problem #252 [Easy].py
@@ -13,3 +13,30 @@
 # --------------------------------------------------------------------------------
 #
 #
+from fractions import Fraction
+import fractions
+
+
+def egyptian_algorithm(fraction, fractions_list=[], i=2):
+    if fraction < 0.0000000000000001:
+        return fractions_list
+    while True:
+        if 1/i <= fraction:
+            break
+        i += 1
+    return egyptian_algorithm(fraction - 1/i, fractions_list+[i], i)
+
+
+def egyptian_algorithm_fraction(fraction, fractions_list=[], i=2):
+    if not fraction:
+        return fractions_list
+    while True:
+        if Fraction(1, i) <= fraction:
+            break
+        i += 1
+    return egyptian_algorithm_fraction(fraction - Fraction(1, i), fractions_list+[Fraction(1, i)], i)
+
+
+assert egyptian_algorithm(4/13) == [4, 18, 468]
+assert egyptian_algorithm_fraction(Fraction(4, 13)) == [
+    Fraction(1, 4), Fraction(1, 18), Fraction(1, 468)]
