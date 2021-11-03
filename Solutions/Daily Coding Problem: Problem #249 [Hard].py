35c6a22165c77388c0feaa91e4d9b207c9f84d9b
pelmenx <vtalantsev@gmail.com>
2021-11-03 16:39:44 +0300

    add #249

--- Solutions/Daily Coding Problem: Problem #249 [Hard].py
+++ Solutions/Daily Coding Problem: Problem #249 [Hard].py
@@ -8,3 +8,15 @@
 # --------------------------------------------------------------------------------
 #
 #
+from sys import maxsize
+
+
+def find_max_xor(array: list[int]) -> int:
+    max_xor = -maxsize
+    for i, item1 in enumerate(array):
+        for item2 in array[i + 1:]:
+            max_xor = max(max_xor, item1 ^ item2)
+    return max_xor
+
+
+assert find_max_xor([1, 2, 3, 4]) == 7
