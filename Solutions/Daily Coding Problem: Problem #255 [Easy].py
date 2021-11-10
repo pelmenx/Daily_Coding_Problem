54feaa8328b29d9e9dd74e7a56d5211b25965ed6
pelmenx <vtalantsev@gmail.com>
2021-11-10 16:03:56 +0300

    add #255

--- Solutions/Daily Coding Problem: Problem #255 [Easy].py
+++ Solutions/Daily Coding Problem: Problem #255 [Easy].py
@@ -30,3 +30,36 @@
 # --------------------------------------------------------------------------------
 #
 #
+def get_matrix1(array):
+    matrix = [[0 for _ in range(len(array))] for _ in range(len(array))]
+    for i, line in enumerate(array):
+        for j in line:
+            matrix[i][j] = 1
+    for i, line in enumerate(matrix):
+        for j, item in enumerate(line):
+            if item and i != j:
+                for k, (item1, item2) in enumerate(zip(matrix[i], matrix[j])):
+                    matrix[i][k] = 1 if item1 or item2 else 0
+    return matrix
+
+
+def get_matrix2(array):
+    def update_matrix(current_row, row):
+        for col in array[current_row]:
+            if col not in visited:
+                matrix[row][col] = 1
+                visited.add(col)
+                update_matrix(col, row)
+
+    matrix = [[0 for _ in range(len(array))] for _ in range(len(array))]
+    visited = set()
+    for i in range(len(array)):
+        update_matrix(i, i)
+        visited.clear()
+    return matrix
+
+
+assert get_matrix1([[0, 1, 3], [1, 2], [2], [3]]) == [
+    [1, 1, 1, 1], [0, 1, 1, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
+assert get_matrix2([[0, 1, 3], [1, 2], [2], [3]]) == [
+    [1, 1, 1, 1], [0, 1, 1, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
